eb33a2d3f9f106e1ee4a637370814dcec0850873
Kevin Camellini <kevincamellini@gmail.com>
2018-01-08 02:48:29 -0500

    users and password page and validation added

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
   get 'users/new'
-
   get 'welcome/index'
-
-  # resources :licenceplates
   resources :licenceplates do
     put :upvote
     put :downvote
   end
-
+  resources :users
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
