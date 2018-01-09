2f586e83c4157cafe3fff31e35ec0770f27932ac
Kevin Camellini <kevincamellini@gmail.com>
2018-01-08 21:30:36 -0500

    added sessions login/logout, validation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   get 'welcome/index'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   resources :licenceplates do
     put :upvote
     put :downvote
