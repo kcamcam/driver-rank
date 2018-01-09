24d135347020f91e8cecd931f82473587ec4325b
Kevin Camellini <kevincamellini@gmail.com>
2018-01-08 22:54:48 -0500

    Finish user edit, update, index, and destroy actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name:  "Kevin Camellini",
+             email: "kevincamellini@gmail.com",
+             password:              "shawarma",
+             password_confirmation: "shawarma",
+             admin: true)
