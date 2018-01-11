9587cea850997dec912161608f30dea0cb3e71b5
Kevin Camellini <kevincamellini@gmail.com>
2018-01-11 00:25:22 -0500

    yay

--- app/models/licenceplate.rb
+++ app/models/licenceplate.rb
@@ -4,5 +4,5 @@ class Licenceplate < ApplicationRecord
                     uniqueness: true,
                     length: { in: 6..7 },
                     format: { with: /\A[a-z]{1}[\d\a-z]{2} {1}[a-z]{3}\Z/i,
-                    message: "is not a valid Quebec licence plate."}
+                    message: "entered is not a valid Quebec licence plate 🤦 <br/> A valid licence plate is: <br/>from [B00 AAA] <br/>to [ZZZ ZZZ] or [Z99 ZZZ]"}
 end
