67cc6b23d582f8afacfc39a7b317179de19d4adc
Kevin Camellini <kevincamellini@gmail.com>
2018-01-15 00:45:13 -0500

    added launch img updated regex for licence plate

--- app/models/licenceplate.rb
+++ app/models/licenceplate.rb
@@ -3,6 +3,6 @@ class Licenceplate < ApplicationRecord
   validates :plate, presence: true,
                     uniqueness: true,
                     length: { in: 6..7 },
-                    format: { with: /\A[a-z]{1}[\d\a-z]{2} {1}[a-z]{3}\Z/i,
+                    format: { with: /\A[\d\a-z]{3} ?[\d\a-z]{3,4}\Z/i,
                     message: "entered is not a valid Quebec licence plate 🤦 <br/> A valid licence plate is: <br/>from [B00 AAA] <br/>to [ZZZ ZZZ] or [Z99 ZZZ]"}
 end
